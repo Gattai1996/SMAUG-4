9259ded9b690910f0f70041d286e9f7d56066902
Gattai1996 <46097759+Gattai1996@users.noreply.github.com>
2020-03-11 09:37:37 -0300

    v0.01
    Player atirando com RayCast invisível

---
+++ New Unity Project/Assets/GolemDamageScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GolemDamageScript : MonoBehaviour
+{
+
+    public float health = 30f;
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log("Health Remaining= " + health);
+
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
